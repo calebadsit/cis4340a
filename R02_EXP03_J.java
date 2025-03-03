ea87bd014bb4c03d669f85e53ade5f8de9fc7997
calebadsit <calebadsit9@gmail.com>
2025-03-02 22:22:16 -0500

    Update R02_EXP03_J.java

--- R02_EXP03_J.java
+++ R02_EXP03_J.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
   
 static Comparator<Integer> cmp = new Comparator<Integer>() {
   public int compare(Integer i, Integer j) {
-    return i < j ? -1 : (i == j ? 0 : 1);
+    return i < j ? -1 : (i > j ? 1 : 0) ;
   }
 };
